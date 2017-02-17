25f2bb98b3976830979644e9a07c9c5ddd63a0ab
Emanuele Cesena <emanuelecesena@pinterest.com>
2017-02-17 14:45:08 -0800

    Add client cmd to list ACLs
    Fixes https://github.com/pinterest/knox/issues/16

---
+++ client/getacl.go
@@ -0,0 +1,44 @@
+package client
+
+import (
+	"fmt"
+	"encoding/json"
+
+	"github.com/pinterest/knox"
+)
+
+func init() {
+	cmdGetACL.Run = runGetACL // break init cycle
+}
+
+var cmdGetACL = &Command{
+	UsageLine: "acl <key_identifier>",
+	Short:     "gets the ACL for a key",
+	Long: `
+Acl get the ACL for a key.
+
+This requires read access to the key and can use user or machine authentication.
+
+For more about knox, see https://github.com/pinterest/knox.
+
+See also: knox keys, knox get
+	`,
+}
+
+func runGetACL(cmd *Command, args []string) {
+	if len(args) != 1 {
+		fatalf("acl takes only one argument. See 'knox help acl'")
+	}
+
+	keyID := args[0]
+	key, err := cli.GetKey(keyID)
+	if err != nil {
+		fatalf("Error getting key: %s", err.Error())
+	}
+	var acl knox.ACL = key.ACL
+
+	for _, a := range acl {
+		aEnc, _ := json.Marshal(a)
+		fmt.Println(string(aEnc))		
+	}
+}

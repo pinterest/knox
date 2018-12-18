40a5fb3de58bc3cb6c7ce278a782befbc2461f22
Jeremy Krach <krach.jeremy.a@gmail.com>
2018-12-18 14:35:34 -0800

    add version command

---
+++ client/version.go
@@ -0,0 +1,18 @@
+package client
+
+import "fmt"
+
+const versionString = "1.2.2"
+
+var cmdVersion = &Command{
+	Run:       runVersion,
+	UsageLine: "version",
+	Short:     "Prints the current version of the Knox client",
+	Long: `
+Prints the current version of the Knox client.
+`,
+}
+
+func runVersion(cmd *Command, args []string) {
+	fmt.Printf("Knox CLI version %s\n", versionString)
+}

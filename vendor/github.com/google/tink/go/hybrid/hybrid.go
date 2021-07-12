665f465e437884005ff7c5f00514ed618fdafcd8
Hao Yang <hyang@pinterest.com>
2021-07-12 20:48:53 +0000

    supporting packages for tink managment

---
+++ vendor/github.com/google/tink/go/hybrid/hybrid.go
@@ -0,0 +1,47 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+// Package hybrid provides subtle implementations of the HKDF and EC primitives.
+//
+// The functionality of Hybrid Encryption is represented as a pair of
+// primitives (interfaces):
+//
+//  * HybridEncrypt for encryption of data
+//
+//  * HybridDecrypt for decryption of data
+//
+// Implementations of these interfaces are secure against adaptive chosen
+// ciphertext attacks. In addition to plaintext the encryption takes an extra
+// parameter contextInfo, which usually is public data implicit from the
+// context, but should be bound to the resulting ciphertext, i.e. the
+// ciphertext allows for checking the integrity of contextInfo (but there are
+// no guarantees wrt. the secrecy or authenticity of contextInfo).
+package hybrid
+
+import (
+	"fmt"
+
+	"github.com/google/tink/go/core/registry"
+)
+
+func init() {
+	if err := registry.RegisterKeyManager(newECIESAEADHKDFPrivateKeyKeyManager()); err != nil {
+		panic(fmt.Sprintf("hybrid.init() failed: %v", err))
+	}
+	if err := registry.RegisterKeyManager(newECIESAEADHKDFPublicKeyKeyManager()); err != nil {
+		panic(fmt.Sprintf("hybrid.init() failed: %v", err))
+	}
+}
---
+++ vendor/github.com/google/tink/go/signature/signature_key_templates.go
@@ -0,0 +1,136 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package signature
+
+import (
+	"github.com/golang/protobuf/proto"
+	commonpb "github.com/google/tink/go/proto/common_go_proto"
+	ecdsapb "github.com/google/tink/go/proto/ecdsa_go_proto"
+	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
+)
+
+// This file contains pre-generated KeyTemplates for Signer and Verifier.
+// One can use these templates to generate new Keysets.
+
+// ECDSAP256KeyTemplate is a KeyTemplate that generates a new ECDSA private key with the following parameters:
+//   - Hash function: SHA256
+//   - Curve: NIST P-256
+//   - Signature encoding: DER
+//   - Output prefix type: TINK
+func ECDSAP256KeyTemplate() *tinkpb.KeyTemplate {
+	return createECDSAKeyTemplate(commonpb.HashType_SHA256,
+		commonpb.EllipticCurveType_NIST_P256,
+		ecdsapb.EcdsaSignatureEncoding_DER,
+		tinkpb.OutputPrefixType_TINK)
+}
+
+// ECDSAP256KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ECDSA private key with the following
+// parameters:
+//   - Hash function: SHA256
+//   - Curve: NIST P-256
+//   - Signature encoding: DER
+//   - Output prefix type: RAW
+func ECDSAP256KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
+	return createECDSAKeyTemplate(commonpb.HashType_SHA256,
+		commonpb.EllipticCurveType_NIST_P256,
+		ecdsapb.EcdsaSignatureEncoding_DER,
+		tinkpb.OutputPrefixType_RAW)
+}
+
+// ECDSAP384KeyTemplate is a KeyTemplate that generates a new ECDSA private key with the following parameters:
+//   - Hash function: SHA512
+//   - Curve: NIST P-384
+//   - Signature encoding: DER
+//   - Output prefix type: TINK
+func ECDSAP384KeyTemplate() *tinkpb.KeyTemplate {
+	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
+		commonpb.EllipticCurveType_NIST_P384,
+		ecdsapb.EcdsaSignatureEncoding_DER,
+		tinkpb.OutputPrefixType_TINK)
+}
+
+// ECDSAP384KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ECDSA private key with the following
+// parameters:
+//   - Hash function: SHA512
+//   - Curve: NIST P-384
+//   - Signature encoding: DER
+//   - Output prefix type: RAW
+func ECDSAP384KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
+	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
+		commonpb.EllipticCurveType_NIST_P384,
+		ecdsapb.EcdsaSignatureEncoding_DER,
+		tinkpb.OutputPrefixType_RAW)
+}
+
+// ECDSAP521KeyTemplate is a KeyTemplate that generates a new ECDSA private key with the following parameters:
+//   - Hash function: SHA512
+//   - Curve: NIST P-521
+//   - Signature encoding: DER
+//   - Output prefix type: TINK
+func ECDSAP521KeyTemplate() *tinkpb.KeyTemplate {
+	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
+		commonpb.EllipticCurveType_NIST_P521,
+		ecdsapb.EcdsaSignatureEncoding_DER,
+		tinkpb.OutputPrefixType_TINK)
+}
+
+// ECDSAP521KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ECDSA private key with the following
+// parameters:
+//   - Hash function: SHA512
+//   - Curve: NIST P-521
+//   - Signature encoding: DER
+//   - Output prefix type: TINK
+func ECDSAP521KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
+	return createECDSAKeyTemplate(commonpb.HashType_SHA512,
+		commonpb.EllipticCurveType_NIST_P521,
+		ecdsapb.EcdsaSignatureEncoding_DER,
+		tinkpb.OutputPrefixType_RAW)
+}
+
+// createECDSAKeyTemplate creates a KeyTemplate containing a EcdasKeyFormat
+// with the given parameters.
+func createECDSAKeyTemplate(hashType commonpb.HashType, curve commonpb.EllipticCurveType,
+	encoding ecdsapb.EcdsaSignatureEncoding, prefixType tinkpb.OutputPrefixType) *tinkpb.KeyTemplate {
+	params := &ecdsapb.EcdsaParams{
+		HashType: hashType,
+		Curve:    curve,
+		Encoding: encoding,
+	}
+	format := &ecdsapb.EcdsaKeyFormat{Params: params}
+	serializedFormat, _ := proto.Marshal(format)
+	return &tinkpb.KeyTemplate{
+		TypeUrl:          ecdsaSignerTypeURL,
+		Value:            serializedFormat,
+		OutputPrefixType: prefixType,
+	}
+}
+
+// ED25519KeyTemplate is a KeyTemplate that generates a new ED25519 private key.
+func ED25519KeyTemplate() *tinkpb.KeyTemplate {
+	return &tinkpb.KeyTemplate{
+		TypeUrl:          ed25519SignerTypeURL,
+		OutputPrefixType: tinkpb.OutputPrefixType_TINK,
+	}
+}
+
+// ED25519KeyWithoutPrefixTemplate is a KeyTemplate that generates a new ED25519 private key.
+func ED25519KeyWithoutPrefixTemplate() *tinkpb.KeyTemplate {
+	return &tinkpb.KeyTemplate{
+		TypeUrl:          ed25519SignerTypeURL,
+		OutputPrefixType: tinkpb.OutputPrefixType_RAW,
+	}
+}
